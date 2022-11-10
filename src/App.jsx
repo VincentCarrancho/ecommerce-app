01004f96a8849a7796eb744ad3868b9495ec09e0
Nicholas de Souza <nrdesouzag13@gmail.com>
2022-11-09 19:41:12 -0500

    Started work on Navbar - Nick

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Hello World and our CEN Class!</div>;
+  return <div className="App">Hello Worls CEN Class!</div>;
 }
 
 export default App;
