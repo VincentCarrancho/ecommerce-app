25d22653a5418224da7b65aaff802802c4518224
Nicholas de Souza <nrdesouzag13@gmail.com>
2022-11-15 21:47:05 -0500

    working on sidebar, need to fix z-index - Nick

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import Navbar from './Components/Navbar'
 import CssBaseline from '@mui/material/CssBaseline';
 import SubNavbar from './Components/SubNavbar'
+import Sidebar from './Components/Sidebar'
 
 
 
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <CssBaseline/>
     <Navbar/>
     <SubNavbar/>
+    <Sidebar/>
   </React.StrictMode>
 )
