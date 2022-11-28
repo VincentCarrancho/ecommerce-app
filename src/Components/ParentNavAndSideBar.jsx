f606a711263083ba4b473de580495fd00fad6d1d
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2022-11-28 11:49:20 -0500

    Implemented data fetching function for. Enabled CORS on port 5173

--- src/Components/ParentNavAndSideBar.jsx
+++ src/Components/ParentNavAndSideBar.jsx
@@ -1,14 +1,32 @@
-import Navbar from './Navbar'
-import SubNavbar from './SubNavbar'
-import Sidebar from './Sidebar'
-
+import Navbar from "./Navbar";
+import SubNavbar from "./SubNavbar";
+import Sidebar from "./Sidebar";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function ParentNavAndSideBar() {
-    return(
-        <div>
-            <Navbar/> 
-            <SubNavbar/>
-            <Sidebar/>
-        </div>
-    )
+  // Testing function. This is how you fetch data in React! Use this as a reference just in case we need to fetch products
+  const [data, setData] = useState();
+  useEffect(() => {
+    // On page load, the app will fetch the data.
+    fetch("http://localhost:8080/products/allProducts", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((obj) => {
+        setData(obj);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <SubNavbar />
+      <Sidebar />
+    </div>
+  );
 }
