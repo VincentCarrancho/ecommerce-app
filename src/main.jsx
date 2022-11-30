792010199027db6436d2aab6b8590916ccebd97a
Nicholas de Souza <nrdesouzag13@gmail.com>
2022-11-29 19:56:42 -0500

    removed unnecessary lines - Nick

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import Navbar from './Components/Navbar'
 import CssBaseline from '@mui/material/CssBaseline';
-// import SubNavbar from './Components/SubNavbar'
-// import Sidebar from './Components/Sidebar'
 import ParentNavandSideBar from './Components/ParentNavAndSideBar'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
     <CssBaseline/>
-    {/* <Navbar/>
-    <SubNavbar/>
-    <Sidebar/> */}
     <ParentNavandSideBar/>
   </React.StrictMode>
 )
